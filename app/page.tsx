2b03e72bad6cc6502d3c6aefa265e86515220cde
[anudeep avula] <anudeepavula009@gmail.com>
2025-05-07 18:39:27 +0530

    feat:video component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import LandingPage from "@/components/landing/index";
 import Features from "@/components/features/index";
+import Video from "@/components/video/VideoComponent";
 
 export default function Home() {
   return (
     <>
       <LandingPage />
+      <div className="aspect-video rounded-lg bg-slate-950 overflow-hidden">
+        <Video />
+      </div>
       <Features />
     </>
   );
